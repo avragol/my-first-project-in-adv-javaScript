166b47e56aebb8d8a7db327ef224779b3690bda9
avrahamGol <avragol@gmail.com>
2023-02-27 11:58:31 +0200

    set modular popup and begining of add pic option

---
+++ src/components/AddNewProperty.js
@@ -0,0 +1,9 @@
+
+const POPUPCONTAINER = document.getElementById("modularPopup")
+
+const initialAddNewPicPopup = () => {
+    POPUPCONTAINER.classList.remove("d-none");
+    POPUPCONTAINER.innerHTML = `
+        
+    `
+}
